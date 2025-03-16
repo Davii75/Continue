ce2c82c0ab4a53a21209c5226ee0583e0b9409b7
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-16 12:36:48 -0300

    Finalização do programa aperte para continuar

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+Console.WriteLine("Obrigado pela atenção!");
+Thread.Sleep(1000);
+Console.WriteLine("Programa finalizado.");
